d06473350d8011561bd3d6ffc51b03f415430461
Djelali DINAUT <ddinaut@e1r13p8.42.fr>
2018-02-14 15:22:47 +0100

    upgrade bzero, strcat done

--- srcs/ft_bzero.s
+++ srcs/ft_bzero.s
@@ -3,18 +3,19 @@ section	.data
 section	.text
 	global	_ft_bzero
 
+;	ft_bzero(void *addr, unsigned int len)
+
 _ft_bzero:
 	push	rbp			; on met en place la stack
 	mov	rbp, rsp		; on save rbp dans rsp
-	xor	rcx, rcx		; on met un registre a 0 aka count
-	mov	rbx, rdi		; on met la chaine dans le registre rbx
+	
+.loop:	
+	dec	rsi			; len--
+	cmp	rsi, 0x0		; si len == 0
+	jl	.ret			; ret
+	mov	byte [rdi + rsi], 0x0	; on met a 0 le byte de rbx aka ptr[len]
+	jmp	.loop			; si != on boucle encore
 
-loop:					; on demare le while
-	mov	byte [rbx], 0	; on met a 0 le byte de rbx aka ptr[x]
-	inc	rbx				; ptr++
-	inc	rcx				; count++
-	cmp	rcx, rsi		; comparaison entre count et le 2e arg de bzero
-	jnl	loop			; si != on boucle encore
-
+.ret:
 	leave
 	ret				; retour
